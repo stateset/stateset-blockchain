4236a1f87687ba2e3a363562588c5fd930850f63
Dom Steil <dominic@dapps-inc.com>
2020-03-29 18:14:15 -0700

    adding fixes

--- x/marketplace/alias.go
+++ x/marketplace/alias.go
@@ -1,17 +1,1 @@
 package marketplace
-
-const (
-	ModuleName   = types.ModuleName
-	RouterKey    = types.RouterKey
-	StoreKey     = types.StoreKey
-	QuerierRoute = types.QuerierRoute
-)
-
-var (
-	NewKeeper    = keeper.NewKeeper
-	BeginBlocker = keeper.BeginBlocker
-)
-
-type (
-	Keeper = keeper.Keeper
-)
