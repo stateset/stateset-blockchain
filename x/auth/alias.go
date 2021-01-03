e87b6b056b83c95c4cae9c00df9be7ceeb318022
Dom Steil <dominic@dapps-inc.com>
2021-01-03 15:11:46 -0800

    adding

--- x/auth/alias.go
+++ x/auth/alias.go
@@ -1,1 +1,14 @@
 package auth
+
+import (
+	"github.com/stateset/stateset-blockchain/x/auth/types"
+)
+
+var (
+	// functions aliases
+	RegisterCodec            = types.RegisterCodec
+	RegisterAccountTypeCodec = types.RegisterAccountTypeCodec
+
+	// variable aliases
+	ModuleCdc = types.ModuleCdc
+)
