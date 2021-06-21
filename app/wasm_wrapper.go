02ddf2b470fba17a0d3c4cb575d8dbe3ea5385ad
Dom Steil <dominic@dapps-inc.com>
2021-06-21 13:25:33 -0700

    adding

--- app/wasm_wrapper.go
+++ app/wasm_wrapper.go
@@ -1,12 +1,9 @@
 package app
 
 import (
-	"github.com/CosmWasm/wasmd/x/wasm"
 	"github.com/cosmos/cosmos-sdk/server"
 )
 
-// WasmWrapper allows us to use namespacing in the config file
-// This is only used for parsing in the app, x/wasm expects WasmConfig
 type WasmWrapper struct {
 	Wasm wasm.Config `mapstructure:"wasm"`
 }
