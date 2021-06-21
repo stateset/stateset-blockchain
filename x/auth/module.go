aa45ac3917a28166122a445d35b0499ece5d7574
Dom Steil <dominic@dapps-inc.com>
2021-06-21 11:12:09 -0700

    adding

--- x/auth/module.go
+++ x/auth/module.go
@@ -9,7 +9,6 @@ import (
 	"github.com/spf13/cobra"
 	abci "github.com/tendermint/tendermint/abci/types"
 
-	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
