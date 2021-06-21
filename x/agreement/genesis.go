6758556c908c23ece06c7b843f83043967c0cf9f
Dom Steil <dominic@dapps-inc.com>
2021-06-21 16:54:13 -0700

    adding

--- x/agreement/genesis.go
+++ x/agreement/genesis.go
@@ -9,19 +9,13 @@ import (
 // DefaultGenesis returns the default Capability genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-		// this line is used by starport scaffolding # ibc/genesistype/default
-		// this line is used by starport scaffolding # genesis/types/default
 		AgreementList: []*Agreement{},
 	}
 }
 
-// Validate performs basic genesis state validation returning an error upon any
-// failure.
-func (gs GenesisState) Validate() error {
-	// this line is used by starport scaffolding # ibc/genesistype/validate
 
-	// this line is used by starport scaffolding # genesis/types/validate
-	// Check for duplicated ID in agreement
+func (gs GenesisState) Validate() error {
+
 	agreementIdMap := make(map[uint64]bool)
 
 	for _, elem := range gs.AgreementList {
--- x/purchaseorder/genesis.go
+++ x/purchaseorder/genesis.go
@@ -8,49 +8,35 @@ import (
 	"github.com/stateset/stateset-blockchain/x/purchaseorder/types"
 )
 
-// GenesisState defines genesis data for the module
-type GenesisState struct {
-	PurchaseOrders []PurchaseOrder `json:"purchaseOrders"`
-	Params         Params          `json:"params"`
-}
-
-// NewGenesisState creates a new genesis state.
-func NewGenesisState() GenesisState {
-	return GenesisState{
-		PurchaseOrders: nil,
-		Params:         DefaultParams(),
+// DefaultGenesis returns the default Capability genesis state
+func DefaultGenesis() *GenesisState {
+	return &GenesisState{
+		PurchaseOrderList: []*PurchaseOrder{},
 	}
 }
 
-// DefaultGenesisState returns a default genesis state
-func DefaultGenesisState() GenesisState { return NewGenesisState() }
 
-// InitGenesis initializes stateset from genesis file
-func InitGenesis(ctx sdk.Context, k Keeper, data GenesisState) {
-	for _, c := range data.PurchaseOrders {
-		k.setPurchaseOrder(ctx, c)
-		k.setPurchaseOrderAgreement(ctx, c.AgreementID, c.PurchaseOrderID)
-	}
-	k.setPurchaseOrderID(ctx, uint64(len(data.PurchaseOrders)+1))
-	k.SetParams(ctx, data.Params)
-}
+func (gs GenesisState) Validate() error {
 
-// ExportGenesis exports the genesis state
-func ExportGenesis(ctx sdk.Context, k Keeper) GenesisState {
-	return GenesisState{
-		PurchaseOrders: k.PurchaseOrders(ctx),
-		Params:          k.GetParams(ctx),
-	}
-}
+	purchaseOrderIdMap := make(map[uint64]bool)
 
-// ValidateGenesis validates the genesis state data
-func ValidateGenesis(data GenesisState) error {
-	if data.Params.MinPurchaseOrderLength < 1 {
-		return fmt.Errorf("Param: MinPurchaseOrderLength must have a positive value")
-	}
-	if data.Params.MaxPurchaseOrderLength < 1 {
-		return fmt.Errorf("Param: MaxPurchaseOrderLength must have a positive value")
+	for _, elem := range gs.PurchaseOrderList {
+		if _, ok := purchaseOrderIdMap[elem.Id]; ok {
+			return fmt.Errorf("duplicated id for agreement")
+		}
+		purchaseOrderIdMap[elem.Id] = true
 	}
 
 	return nil
+}
+
+// ExportGenesis returns the capability module's exported genesis.
+func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
+	genesis := types.DefaultGenesis()
+
+	// this line is used by starport scaffolding # genesis/module/export
+
+	genesis.PortId = k.GetPort(ctx)
+
+	return genesis
 }
