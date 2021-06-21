402f6704af63c301e4a497f14360a06d9d114377
Dom Steil <dominic@dapps-inc.com>
2021-06-21 14:13:04 -0700

    adding

--- x/wasm/internal/types/msg_binding.go
+++ x/wasm/internal/types/msg_binding.go
@@ -3,8 +3,6 @@ package types
 import (
 	"encoding/json"
 
-	wasmTypes "github.com/CosmWasm/wasmd/types"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
