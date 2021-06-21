c5221d6fc8a5b982d4a47f30896f768e799307aa
Dom Steil <dominic@dapps-inc.com>
2021-06-21 15:33:05 -0700

    adding

--- x/invoice/genesis.go
+++ x/invoice/genesis.go
@@ -1,23 +1,23 @@
-package purchaseorder
+package invoice
 
 import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/stateset/stateset-blockchain/x/purchaseorder/keeper"
-	"github.com/stateset/stateset-blockchain/x/purchaseorder/types"
+	"github.com/stateset/stateset-blockchain/x/invoice/keeper"
+	"github.com/stateset/stateset-blockchain/x/invoice/types"
 )
 
 // GenesisState defines genesis data for the module
 type GenesisState struct {
-	PurchaseOrders []PurchaseOrder `json:"purchaseOrders"`
+	Invoices []Invoice `json:"invoices"`
 	Params         Params          `json:"params"`
 }
 
 // NewGenesisState creates a new genesis state.
 func NewGenesisState() GenesisState {
 	return GenesisState{
-		PurchaseOrders: nil,
+		Invoices: nil,
 		Params:         DefaultParams(),
 	}
 }
@@ -27,29 +27,29 @@ func DefaultGenesisState() GenesisState { return NewGenesisState() }
 
 // InitGenesis initializes stateset from genesis file
 func InitGenesis(ctx sdk.Context, k Keeper, data GenesisState) {
-	for _, c := range data.PurchaseOrders {
-		k.setPurchaseOrder(ctx, c)
-		k.setPurchaseOrderAgreement(ctx, c.AgreementID, c.PurchaseOrderID)
+	for _, c := range data.Invoices {
+		k.setInvoices(ctx, c)
+		k.setInvoiceAgreement(ctx, c.AgreementID, c.InvoiceID)
 	}
-	k.setPurchaseOrderID(ctx, uint64(len(data.PurchaseOrders)+1))
+	k.setInvoiceID(ctx, uint64(len(data.Invoices)+1))
 	k.SetParams(ctx, data.Params)
 }
 
 // ExportGenesis exports the genesis state
 func ExportGenesis(ctx sdk.Context, k Keeper) GenesisState {
 	return GenesisState{
-		PurchaseOrders: k.PurchaseOrders(ctx),
+		Invoices: k.Invoices(ctx),
 		Params:          k.GetParams(ctx),
 	}
 }
 
 // ValidateGenesis validates the genesis state data
 func ValidateGenesis(data GenesisState) error {
-	if data.Params.MinPurchaseOrderLength < 1 {
-		return fmt.Errorf("Param: MinPurchaseOrderLength must have a positive value")
+	if data.Params.MinInvoiceLength < 1 {
+		return fmt.Errorf("Param: MinInvoiceLength must have a positive value")
 	}
-	if data.Params.MaxPurchaseOrderLength < 1 {
-		return fmt.Errorf("Param: MaxPurchaseOrderLength must have a positive value")
+	if data.Params.MaxInvoiceLength < 1 {
+		return fmt.Errorf("Param: MaxInvoiceLength must have a positive value")
 	}
 
 	return nil
