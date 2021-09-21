c035e51ae2f68904e3b66944594a6a1ee73d056b
Dom Steil <dominic@dapps-inc.com>
2021-09-20 20:09:27 -0700

    updating proto

--- x/agreement/types/events.go
+++ x/agreement/types/events.go
@@ -3,7 +3,7 @@ package types
 const (
 	TypeEvtAgreementCreated   = "agreement_created"
 	TypeEvtAgreementActivated   = "agreement_activated"
-	TypeEvtAgreementAmended= "agreement_amended"
+	TypeEvtAgreementAmended = "agreement_amended"
 	TypeEvtAgreementRenewed  = "agreement_renewed"
 	TypeEvtAgreementExpired = "agreement_expired"
 	TypeEvtAgreementTerminated = "agreement_terminated"
