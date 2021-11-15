58ec64cbcb8695e54c1c6c909eb5fb74dc12626c
Dom Steil <dominic@dapps-inc.com>
2021-11-15 04:50:20 -0800

    adding loan handlet

--- x/loan/keeper/expected_keepers.go
+++ x/loan/keeper/expected_keepers.go
@@ -9,3 +9,10 @@ type BankKeeper interface {
 	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 }
+
+// DidKeeper defines the expected interface needed to add dids.
+type DidKeeper interface {
+	AddDid(ctx sdk.Context, did) error
+	GetDid(ctx sdk.Context, did) error
+	AddCredentials(ctx sdk.Context, did, credentials) error
+}
---
+++ x/loan/keeper/msg_server_deposit_collateral.go
@@ -0,0 +1,40 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/stateset/stateset-blockchain/x/loan/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (k msgServer) DepositCollateral(goCtx context.Context, msg *types.MsgDepositCollateral) (*types.MsgDepositCollateralResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	loan, found := k.GetLoan(ctx, msg.Id)
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
+	}
+	
+	// Get the borrower address
+	borrower, _ := sdk.AccAddressFromBech32(loan.Creator)
+
+	// Get the collateral as sdk.Coins
+	collateral, err := sdk.ParseCoinsNormalized(loan.Collateral)
+	if err != nil {
+		panic(err)
+	}
+
+	// Use the module account as escrow account
+	sdkError := k.bankKeeper.SendCoinsFromAccountToModule(ctx, borrower, types.ModuleName, collateral)
+	if sdkError != nil {
+		return nil, sdkError
+	}
+
+	// Add the loan to the keeper
+	k.AppendLoan(
+		ctx,
+		loan,
+	)
+
+	return &types.MsgDepositCollateralResponse{}, nil
+}
